3b64363677d0c53ec0086a66cc91148684ddafc4
iOSMoonSand <alexisschreier@gmail.com>
2016-05-18 15:16:58 -0700

    Create Blocstagram repo and Xcode project

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Alexis Schreier on 05/18/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Alexis Schreier on 05/18/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
