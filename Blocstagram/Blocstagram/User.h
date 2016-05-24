4062aa24befa4beada6d2eca1eeff78e1e253fb2
iOSMoonSand <alexisschreier@gmail.com>
2016-05-24 15:35:20 -0700

    Checkpoint 35 completed

--- Blocstagram/Blocstagram/User.h
+++ Blocstagram/Blocstagram/User.h
@@ -8,7 +8,7 @@
 
 #import <UIkit/UIKit.h>
 
-@interface User : NSObject
+@interface User : NSObject <NSCoding>
 
 @property (nonatomic, strong) NSString *idNumber;
 @property (nonatomic, strong) NSString *userName;
