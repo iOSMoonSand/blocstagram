1799b47ed4f6522a80dced81982d8070599d68ba
iOSMoonSand <alexisschreier@gmail.com>
2016-06-17 14:24:06 -0700

    Checkpoint 45 completed

---
+++ Blocstagram/BlocstagramTests/BlocstagramTests.m
@@ -0,0 +1,44 @@
+//
+//  UserTests.m
+//  Blocstagram
+//
+//  Created by Alexis Schreier on 06/17/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "User.h"
+
+@interface UserTests : XCTestCase
+
+@end
+
+@implementation UserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testThatInitializationWorks {
+    
+    NSDictionary *sourceDictionary = @{@"id": @"8675309",
+                                       @"username" : @"d'oh",
+                                       @"full_name" : @"Homer Simpson",
+                                       @"profile_picture" : @"http://www.example.com/example.jpg"};
+    
+    User *testUser = [[User alloc] initWithDictionary:sourceDictionary];
+    
+    XCTAssertEqualObjects(testUser.idNumber, sourceDictionary[@"id"], @"The id number should be equal");
+    XCTAssertEqualObjects(testUser.userName, sourceDictionary[@"username"], @"The username shouled be equal");
+    XCTAssertEqualObjects(testUser.fullName, sourceDictionary[@"full_name"], @"The full name should be equal");
+    XCTAssertEqualObjects(testUser.profilePictureURL, [NSURL URLWithString:sourceDictionary[@"profile_picture"]], @"The profile picture should be equal");
+}
+
+@end
+
